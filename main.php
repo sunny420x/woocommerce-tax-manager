b9d6249553352ca181082353ae31fc08c76c082c
Sunny Jirakit <jirakitpawnsakunrungrot@gmail.com>
2026-07-11 13:20:59 +0700

    feat: white-label-zone update

--- main.php
+++ main.php
@@ -89,7 +89,7 @@ a.menu-btn {
 }
 </style>
 <div class="white-label-zone no-print">
-    <span style="padding: 60px 10px 60px 40px;float: left;font-size: 60px;">🧾</span>
+    <span style="padding: 40px 10px 40px 40px;float: left;font-size: 60px;">🧾</span>
     <div style="padding: 20px 0;">
         <h1>WordPress Tax & Fee Manager</h1>
         <p>
